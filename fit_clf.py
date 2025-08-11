214dcc86f388a80063828381772d4fc81336c607
Baturay Saglam <baturaysaglam@gmail.com>
2025-08-11 20:41:40 +0000

    Added scripts to train MPL clf on hidden states

---
+++ fit_clf.py
@@ -0,0 +1,145 @@
+import argparse
+import json
+import os
+import uuid
+
+import numpy as np
+import torch
+from transformers import AutoConfig
+
+from utils.clf_eval import evaluate_classification
+from utils.mlp import *
+from utils.utils import hf_model_name_dict, load_hidden_states
+
+SEED = 42
+
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+
+
+def main(args):
+    args.seed = SEED
+    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+    
+    exp_dir = os.path.join('clf_models',
+                           args.model,
+                           f'layer_{args.layer}',
+                           args.dataset,
+                           uuid.uuid4().hex[:8])
+    os.makedirs(exp_dir, exist_ok=True)
+
+    model_conf = AutoConfig.from_pretrained(hf_model_name_dict[args.model])
+
+    topics = [
+        'vanilla_benign',
+        'vanilla_harmful',
+        'adversarial_benign',
+        'adversarial_harmful'
+    ] if 'wild' in args.dataset else [topic.split('.csv') for topic in os.listdir(f'./datasets/{args.dataset}') if topic.endswith('.csv')]
+
+    # Load the topics
+    hidden_states, cls_count = {}, {}
+    for i, topic in enumerate(topics):
+        X = load_hidden_states(args.model,
+                               args.layer,
+                               args.dataset,
+                               topic=topic,
+                               embed_dim=model_conf.hidden_size,
+                               device=device)
+        hidden_states[topic] = X
+        cls_count[str(i)] = X.shape[0]
+
+    X = torch.cat([hidden_states[type] for type in topics], dim=0,).to(device)
+    y = torch.tensor([i for i, type in enumerate(topics) for _ in range(hidden_states[type].shape[0])], device=device)
+
+    clf = MLP(
+        d_in=model_conf.hidden_size,
+        n_cls=len(hidden_states),
+        hidden_size=args.mlp_layers,
+        dropout_p=args.dropout,
+        use_ln=args.use_ln
+    ).to(device)
+    final_clf = fit(clf, X, y, args, eval_ratio=0.1, early_stop_tol=5)
+
+    # Load the test set
+    hidden_states_test, cls_count = {}, {}
+    for i, topic in enumerate(topics):
+        X = load_hidden_states(args.model,
+                               args.layer,
+                               f"{args.dataset}/eval",
+                               topic=topic,
+                               embed_dim=model_conf.hidden_size,
+                               device=device)
+        hidden_states_test[topic] = X
+        cls_count[str(i)] = X.shape[0]
+
+    X_test = torch.cat([hidden_states_test[type] for type in topics], dim=0,).to(device)
+    y_test = torch.tensor([i for i, type in enumerate(topics) for _ in range(hidden_states_test[type].shape[0])], device=device)
+
+    y_pred = final_clf.predict(X_test)
+    test_metrics = evaluate_classification(y_test.cpu().numpy(), y_pred.cpu().numpy())
+
+    torch.save(final_clf.state_dict(), f'{exp_dir}/clf.pth')
+
+    with open(f'{exp_dir}/args.json', "w") as f:
+        json.dump(vars(args), f, indent=4)
+
+    with open(f'{exp_dir}/result.json', "w") as f:
+        json.dump(test_metrics, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate linear separability between two topics in hidden space using a linear SVM.")
+    # Data arguments
+    parser.add_argument('--model',
+                        type=str,
+                        default='llama-3.1-8b-instruct',
+                        help="The Hugging Face model name or path should be entered in lowercase after the first `/` in the official model name, e.g., `llama-3.1-8b`.")
+    parser.add_argument('--layer',
+                        type=int,
+                        default=32,
+                        help="Layer number to extract hidden states from. Uses 1-based indexing, e.g., to get the 17-th layer, set this to 17.")
+    parser.add_argument('--dataset',
+                        type=str,
+                        default='wildjb',
+                        choices=['arxiv',
+                                 'cot',
+                                 'wildjb',
+                                 'wildguardmix'],
+                        help="Name of the dataset. It's assumed to locate under './datasets'")
+    
+    # Training arguments
+    parser.add_argument('--num_epochs',
+                        type=int,
+                        default=40,
+                        help="Number of training epochs.")
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=2048,
+                        help="Batch size for training.")
+    parser.add_argument('--mlp_layers',
+                        type=int,
+                        nargs='+',
+                        default=[2048, 2048, 512, 512, 64],
+                        help="Number of layers in the MLP.")
+    parser.add_argument('--lr',
+                        type=float,
+                        default=7.55e-4,
+                        help="Learning rate for the optimizer.")
+    parser.add_argument('--dropout',
+                        type=float,
+                        default=0.1,
+                        help="Dropout rate for the model.")
+    parser.add_argument('--use_ln',
+                        action='store_true',
+                        help="Use layer normalization.")
+
+    # Misc
+    parser.add_argument('--gpu',
+                    type=int,
+                    default=0,
+                    help="Ordinal of the GPU to use. Default is 0.")
+    
+    args = parser.parse_args()
+
+    main(args)
--- utils/clf_eval.py
+++ utils/clf_eval.py
@@ -1,34 +1,33 @@
 import numpy as np
 import pandas as pd
-import torch
-import torch.nn.functional as F
 from sklearn.metrics import (accuracy_score, classification_report,
                              confusion_matrix, f1_score, roc_auc_score)
 
 
-# Let's set:
 # Label 0 --> Vanilla Benign
 # Label 1 --> Vanilla Harmful
 # Label 2 --> Adversarial Benign
 # Label 3 --> Adversarial Harmful
-def evaluate_classification(y_true, y_pred):
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
+def evaluate_classification(y_true: np.ndarray, y_pred: np.ndarray) -> dict:
+    n_cls = len(np.unique(y_true))
+    labels = np.arange(0, n_cls)
 
-    labels = np.arange(0, 4)
-    legend = [
-        'van_benign',
-        'van_harmful',
-        'adv_benign',
-        'adv_harmful'
-    ]
-
-    acc = accuracy_score(y_true, y_pred)
-    
     # Confusion matrix
+    acc = accuracy_score(y_true, y_pred)
+    auc = group_auc(y_true, y_pred, group=(1, 3))
     cm = confusion_matrix(y_true, y_pred, labels=labels)
-    
-    # Adversarial macro F1 (only for adversarial classes), handle zero divisions gracefully
+
+    # Malicious macro F1 (only for malicious classes), handle zero divisions gracefully
+    mal_labels = [1, 3]
+    mal_macro_f1 = f1_score(
+        y_true, 
+        y_pred, 
+        labels=mal_labels, 
+        average='macro', 
+        zero_division=0
+    )
+
+    # Adversarial macro F1 (only for adversarial classes)
     adv_labels = [2, 3]
     adv_macro_f1 = f1_score(
         y_true, 
@@ -46,7 +45,10 @@ def evaluate_classification(y_true, y_pred):
     
     print("Confusion Matrix:")
     print(pd.DataFrame(cm, index=labels, columns=labels), "\n")
+    
+    print(f"Harmful Macro F1: {mal_macro_f1:.4f}")
     print(f"Adversarial Macro F1: {adv_macro_f1:.4f}\n")
+
     print("Classification Report:")
     print(classification_report(
         y_true, 
@@ -55,23 +57,25 @@ def evaluate_classification(y_true, y_pred):
         zero_division=0
     ))
 
+    return {
+        'accuracy': acc,
+        'harmful_macro_f1': mal_macro_f1,
+        'adv_macro_f1': adv_macro_f1,
+        'auc': auc,
+        'conf_mat': cm.tolist()
+    }
+
 
 def group_auc(
-    logits: torch.Tensor,
-    labels: torch.Tensor,
-    group_a: tuple[int, ...] = (0, 2),   # positive
-    group_b: tuple[int, ...] = (1, 3),   # negative
+    y_true: np.ndarray,
+    y_pred: np.ndarray,
+    group: tuple[int, ...] = (1, 3),   # negative
 ) -> float:
     """
     ROC-AUC for separating group_a vs. group_b.
     group_a is treated as the positive class.
     """
-    # P(class ∈ group_a)  –> score for positive class
-    probs = F.softmax(logits, dim=-1)
-    p_pos = probs[:, list(group_a)].sum(dim=1).cpu().numpy()
+    yt = np.isin(y_true, group).astype(int)
+    yp = np.isin(y_pred, group).astype(int)
 
-    # 1 if label in group_a else 0
-    y_true = torch.isin(labels, torch.tensor(group_a, device=labels.device)).long()
-    y_true = y_true.cpu().numpy()
-
-    return roc_auc_score(y_true, p_pos)
+    return roc_auc_score(yt, yp)
